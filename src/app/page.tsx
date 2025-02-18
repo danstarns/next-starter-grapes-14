5d65173f54731bec424036d96da4a25f90f853a0
danstarns <danielstarns@hotmail.com>
2025-02-18 12:10:25 +0000

    feat: add next 14 grapes

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import grapesjs, { Editor } from 'grapesjs';
+import GjsEditor from '@grapesjs/react';
+
+export default function DefaultEditor() {
+  const onEditor = (editor: Editor) => {
+    console.log('Editor loaded', { editor });
+  };
+
+  return (
+    <GjsEditor
+      // Pass the core GrapesJS library to the wrapper (required).
+      // You can also pass the CDN url (eg. "https://unpkg.com/grapesjs")
+      grapesjs={grapesjs}
+      // Load the GrapesJS CSS file asynchronously from URL.
+      // This is an optional prop, you can always import the CSS directly in your JS if you wish.
+      // GrapesJS init options
+      options={{
+        height: '100vh',
+        storageManager: false,
+      }}
+      onEditor={onEditor}
+    />
+  );
+}
